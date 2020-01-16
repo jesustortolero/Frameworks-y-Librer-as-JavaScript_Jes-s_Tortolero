4d8e01920c13a7409a3a78f7837d73395a1cb9bb
jesús <jgta1994@yahoo.com>
2020-01-16 16:24:40 -0300

    segundo commit
    se hizo la iteración para la búsqueda y el marcado de los dulces para que luego se puedan eliminar, también se hizo le animación al momento de eliminar y la eliminación de los caramelos, se termino el cronometro. se hizo la función para agregar los dulces faltaría la animación y el conteo de movimientos.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 $( document ).ready(function(){
 
-    //multiplicacion de string
+    //-------- definicion de variables -------
+
+    var tiempo=0;
+
+    var resHorizonatal = 0;
+    var resVertical = 0;
+    var cuenta = 0;
+    var movimiento = 0;
+
+
+
+    //-------------multiplicacion de string-------------
     String.prototype.times = function(n) {
         return Array.prototype.join.call({length: n+1},this);
         
@@ -11,7 +22,8 @@ $( document ).ready(function(){
     
         $("[class*='col']").append(etiqueta);
 
-        //ruta img
+        //-------------ruta img-------------
+
         $(".caramelo").attr("src",function(){
             var aleatorio = Math.floor(Math.random()*4)+1;
             var ruta_img = "image/"+ aleatorio+".png";
@@ -29,258 +41,228 @@ $( document ).ready(function(){
 
     };
 
-    //animacion de color
-    function anim(){
-        var primero = "#DCFF0E";
+    //-------------animacion de color-------------
+
+      function anim(){  
+    var primero = "#DCFF0E";
         var segundo = "#BF2DE3";
+
         $(".main-titulo").animate({color: primero}, 1000, function(){
             $(".main-titulo").animate({color: segundo}, 1000, function(){
                 anim() 
             });
-        });
+        })};
+
+
+    //------------- funcion de tiempo -------------
+
+    var min = 2
+    var seg = 0
+
+    function timer(){
+        if(seg!=0){
+            seg=seg-1;}
+        if(seg==0){
+            if(min==0){
+                clearInterval(eliminar);
+                //clearInterval(nuevosDulces);
+                //clearInterval(intervalo);
+                clearInterval(tiempo);
+                $(".panel-tablero").hide("drop","slow",/*funcioncita*/);
+                $(".time").hide();
+            }
+            seg=59;
+            min=min-1;};
+
+            if(seg.toString().length < 2){
+                $("#timer").html("0"+min+":"+"0"+seg)
+            }else{
+                $("#timer").html("0"+min+":"+seg)
+            }
+            
+            
+            
     };
 
+
+    //---------- filas y columnas-------
+
+    function validar(){
+        // horizontal
+        var horizontal = false;
+        var elementHor = '';
+        var elementVer = '';
+        for(var j=1;j<8;j++){
+            for(var k=1;k<6;k++){
+                var res1=$(".col-"+k).children("img:nth-child("+j+")").attr("src");
+                var res2=$(".col-"+(k+1)).children("img:nth-child("+j+")").attr("src");
+                var res3=$(".col-"+(k+2)).children("img:nth-child("+j+")").attr("src");
     
+                if (res1 == res2 && res2 == res3) {                    
+                    $(".col-" + k).children("img:nth-child(" + (j) + ")").attr("class", "elemento activo");
+                    $(".col-" + (k + 1)).children("img:nth-child(" + (j) + ")").attr("class", "elemento activo");
+                    $(".col-" + (k + 2)).children("img:nth-child(" + (j) + ")").attr("class", "elemento activo");
 
-// boton inicio
+                    resHorizonatal+=10;
+                    console.log("resultado horizontal-"+resHorizonatal);
+                    
+                };
+            }};
 
-$(".btn-reinicio").click(function(){
-    
-    
+        //  ----------vertical----------
 
-    if($(".btn-reinicio").text()=="Iniciar"){
+         for (var l = 1; l < 6; l++) {
+             for (var k = 1; k < 8; k++) {
+                 var res1 = $(".col-" + k).children("img:nth-child(" + l + ")").attr("src");
+                 var res2 = $(".col-" + k).children("img:nth-child(" + (l + 1) + ")").attr("src");
+                 var res3 = $(".col-" + k).children("img:nth-child(" + (l + 2) + ")").attr("src");
 
-        // obtiene filas de dulces o columas
-        function giveCandyArrays(arrayType, index) {
+                 if (res1 == res2 && res2 == res3) {
+                     $(".col-" + k).children("img:nth-child(" + (l) + ")").attr("class", "elemento activo");
+                     $(".col-" + k).children("img:nth-child(" + (l + 1) + ")").attr("class", "elemento activo");
+                     $(".col-" + k).children("img:nth-child(" + (l + 2) + ")").attr("class", "elemento activo");
 
-            var candyCol1 = $('.col-1').children();
-            var candyCol2 = $('.col-2').children();
-            var candyCol3 = $('.col-3').children();
-            var candyCol4 = $('.col-4').children();
-            var candyCol5 = $('.col-5').children();
-            var candyCol6 = $('.col-6').children();
-            var candyCol7 = $('.col-7').children();
+                     resVertical+=10;
+                     console.log("resultado vertical-"+resVertical);
+                     
+                 };
+             }};
 
-            var candyColumns = $([candyCol1, candyCol2, candyCol3, candyCol4,
-                candyCol5, candyCol6, candyCol7
-            ]);
+        var resFinal = resVertical + resHorizonatal;
+        console.log("resultaddo final -"+resFinal);
 
-            if (typeof index === 'number') {
-                var candyRow = $([candyCol1.eq(index), candyCol2.eq(index), candyCol3.eq(index),
-                    candyCol4.eq(index), candyCol5.eq(index), candyCol6.eq(index),
-                    candyCol7.eq(index)
-                ]);
-            } else {
-                index = '';
-            }
+        if(resFinal > 0){
+            
+            cuenta += resFinal;
+            console.log("cuenta-"+cuenta)
+            $("#score-text").html(cuenta)
+            resHorizonatal = 0;
+            resVertical = 0;
+            resFinal = 0;
 
-            if (arrayType === 'columns') {
-                return candyColumns;
-            } else if (arrayType === 'rows' && index !== '') {
-                return candyRow;
-            }
-        }
-
-        // arreglos de filas
-        function candyRows(index) {
-            var candyRow = giveCandyArrays('rows', index);
-            return candyRow;
-        }
-
-        // arreglos de colunmnas
-        function candyColumns(index) {
-            var candyColumn = giveCandyArrays('columns');
-            return candyColumn[index];
-        }
-
-
-
-        //Valida si hay dulces par eliminar en una columna
-        function columnValidation() {
-            for (var j = 0; j < 7; j++) {
-                var counter = 0;
-                var candyPosition = [];
-                var extraCandyPosition = [];
-                var candyColumn = candyColumns(j);
-                var comparisonValue = candyColumn.eq(0);
-                var gap = false;
-                for (var i = 1; i < candyColumn.length; i++) {
-                    var srcComparison = comparisonValue.attr('src');
-                    var srcCandy = candyColumn.eq(i).attr('src');
-
-                    if (srcComparison != srcCandy) {
-                        if (candyPosition.length >= 3) {
-                            gap = true;
-                        } else {
-                            candyPosition = [];
-                        }
-                        counter = 0;
-                    } else {
-                        if (counter == 0) {
-                            if (!gap) {
-                                candyPosition.push(i - 1);
-                            } else {
-                                extraCandyPosition.push(i - 1);
-                            }
-                        }
-                        if (!gap) {
-                            candyPosition.push(i);
-                        } else {
-                            extraCandyPosition.push(i);
-                        }
-                        counter += 1;
-                    }
-                    comparisonValue = candyColumn.eq(i);
-                }
-                if (extraCandyPosition.length > 2) {
-                    candyPosition = $.merge(candyPosition, extraCandyPosition);
-                }
-                if (candyPosition.length <= 2) {
-                    candyPosition = [];
-                }
-                candyCount = candyPosition.length;
-                if (candyCount >= 3) {
-                    deleteColumnCandy(candyPosition, candyColumn);
-                    setScore(candyCount);
-                }
-            }
-        }
-        function  deleteColumnCandy(candyPosition, candyColumn) {
-            for (var i = 0; i < candyPosition.length; i++) {
-                candyColumn.eq(candyPosition[i]).addClass('delete');
-            }
-        }
-
-        // Valida si hay dulces par eliminar en la fila
-        function rowValidation() {
-            for (var j = 0; j < 6; j++) {
-                var counter = 0;
-                var candyPosition = [];
-                var extraCandyPosition = [];
-                var candyRow = candyRows(j);
-                var comparisonValue = candyRow[0];
-                var gap = false;
-                for (var i = 1; i < candyRow.length; i++) {
-                    var srcComparison = comparisonValue.attr('src');
-                    var srcCandy = candyRow[i].attr('src');
-
-                    if (srcComparison != srcCandy) {
-                        if (candyPosition.length >= 3) {
-                            gap = true;
-                        } else {
-                            candyPosition = [];
-                        }
-                        counter = 0;
-                    } else {
-                        if (counter == 0) {
-                            if (!gap) {
-                                candyPosition.push(i - 1);
-                            } else {
-                                extraCandyPosition.push(i - 1);
-                            }
-                        }
-                        if (!gap) {
-                            candyPosition.push(i);
-                        } else {
-                            extraCandyPosition.push(i);
-                        }
-                        counter += 1;
-                    }
-                    comparisonValue = candyRow[i];
-                }
-                if (extraCandyPosition.length > 2) {
-                    candyPosition = $.merge(candyPosition, extraCandyPosition);
-                }
-                if (candyPosition.length <= 2) {
-                    candyPosition = [];
-                }
-                candyCount = candyPosition.length;
-                if (candyCount >= 3) {
-                    deleteHorizontal(candyPosition, candyRow);
-                    setScore(candyCount);
-                }
-            }
-        }
-        function deleteHorizontal(candyPosition, candyRow) {
-            for (var i = 0; i < candyPosition.length; i++) {
-                candyRow[candyPosition[i]].addClass('delete');
-            }
-        }
-
-        //  interaccion caramelos
-        function addCandyEvents() {
-            $('img').draggable({
-                containment: '.panel-tablero',
-                droppable: 'img',
-                revert: true,
-                revertDuration: 500,
-                grid: [100, 100],
-                zIndex: 10,
-                drag: constrainCandyMovement
-            });
-            $('img').droppable({
-                drop: swapCandy
-            });
-            enableCandyEvents();
+            console.log("horizontal 2-"+ resHorizonatal)
+            console.log("vertical 2-"+ resVertical)
+            console.log("Final 2-"+ resFinal)
         }
         
-        function disableCandyEvents() {
-            $('img').draggable('disable');
-            $('img').droppable('disable');
-        }
         
-        function enableCandyEvents() {
-            $('img').draggable('enable');
-            $('img').droppable('enable');
-        }
+
+        eliminar();
+    };
+
+    function eliminar(){
         
-        //hace que el caramelo sea solido al moverse
-        function constrainCandyMovement(event, candyDrag) {
-            candyDrag.position.top = Math.min(100, candyDrag.position.top);
-            candyDrag.position.bottom = Math.min(100, candyDrag.position.bottom);
-            candyDrag.position.left = Math.min(100, candyDrag.position.left);
-            candyDrag.position.right = Math.min(100, candyDrag.position.right);
-        }
-        
-        //reemplaza a los caramelos anteriores
-        function swapCandy(event, candyDrag) {
-            var candyDrag = $(candyDrag.draggable);
-            var dragSrc = candyDrag.attr('src');
-            var candyDrop = $(this);
-            var dropSrc = candyDrop.attr('src');
-            candyDrag.attr('src', dropSrc);
-            candyDrop.attr('src', dragSrc);
-        
-            setTimeout(function () {
-                checkBoard();
-                if ($('img.delete').length === 0) {
-                    candyDrag.attr('src', dragSrc);
-                    candyDrop.attr('src', dropSrc);
-                } else {
-                    updateMoves();
-                }
-            }, 500);
-        
-        }
-
-
-        $(".btn-reinicio").text("Reiniciar");
-
-        $('#timer').startTimer({
-			onComplete: endGame
-		})
-
-    }else if($(".btn-reinicio").text()=="Reiniciar"){
-
-        eliminaElementos()
-
-        agregarElemento()
-        
-        $(".btn-reinicio").text("Iniciar");
-
+        $(".elemento.activo").hide("pulsate",1000,function(){
+            $(".elemento.activo").remove();
+            agregarDulcesFaltantes();
+		});
     }
-});
+
+    function agregarDulcesFaltantes(){
+        for(var k=1;k<8;k++){
+            var res=$(".col-"+k).children().length;
+            console.log("En col "+k+" - Cantidad "+res);
+            if(res < 7){
+                for(var l = res; l<7; l++){
+                    var aleatorio = Math.floor(Math.random()*4)+1;
+                    var ruta_img = "image/"+ aleatorio+".png";
+                    var etiqueta = "<img class='caramelo elemento' src='"+ruta_img+"'/>";
+                    $(".col-"+k).prepend(etiqueta).slideDown("slow");
+                    $(".caramelo").draggable({
+                        containment: '.panel-tablero',
+                    droppable: 'img',
+                    revert: true,
+                    revertDuration: 500,
+                    grid: [100, 100],
+                    zIndex: 10,
+                    stop: validar                        
+                    });
+                    
+                    $('img').droppable({
+                        drop: swapCandy            
+                });
+                function swapCandy(event, candyDrag) {
+                    var candyDrag = $(candyDrag.draggable);
+                    var dragSrc = candyDrag.attr('src');
+                    var candyDrop = $(this);
+                    var dropSrc = candyDrop.attr('src');
+                    candyDrag.attr('src', dropSrc);
+                    candyDrop.attr('src', dragSrc);
+                };
+                       
+                }
+            }
+            
+            
+        }
+        validar()
+    }
+
+    //-------------boton inicio-------------
+
+    $(".btn-reinicio").click(function(){   
+
+        // if(swapCandy()){
+
+        //     movimiento += 1;
+        //      console.log("movimientos-"+movimiento)
+
+        // }
+        
+        
+        if($(".btn-reinicio").text()=="Iniciar"){
+            anim()
+            validar()
+
+            //------------- interaccion caramelos-------------
+                $('img').draggable({
+                    containment: '.panel-tablero',
+                    droppable: 'img',
+                    revert: true,
+                    revertDuration: 500,
+                    grid: [100, 100],
+                    zIndex: 10,
+                    stop: validar
+                });
+                $('img').droppable({
+                    drop: swapCandy            
+            });
+
+            function swapCandy(event, candyDrag) {
+                var candyDrag = $(candyDrag.draggable);
+                var dragSrc = candyDrag.attr('src');
+                var candyDrop = $(this);
+                var dropSrc = candyDrop.attr('src');
+                candyDrag.attr('src', dropSrc);
+                candyDrop.attr('src', dragSrc);
+            };        
+
+
+            $(".btn-reinicio").text("Reiniciar");
+
+            tiempo=setInterval(function(){
+                timer()
+            },1000); 
+
+
+        }else if($(".btn-reinicio").text()=="Reiniciar"){
+
+            $(".panel-tablero, .time").show("slow")
+            $("#score-text").html(0)
+
+            cuenta = 0;
+
+            clearInterval(tiempo)
+
+            eliminaElementos()
+
+            agregarElemento()
+
+            $("#timer").html("02:00")
+            
+            $(".btn-reinicio").text("Iniciar");
+
+        }
+    });
     
 });
-
-
